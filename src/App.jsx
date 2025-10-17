a2186af6f2bc274cb5b1bb659aa0794de7eb6759
DennisKodehode <dennis.kodehode@gmail.com>
2025-10-17 12:01:55 +0200

    switched router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import { FavoritePage } from "./pages/FavoritePage";
 import { DetailsPage } from "./pages/DetailsPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/book/:id" element={<DetailsPage />} />
         <Route path="/favorites" element={<FavoritePage />} />
         <Route path="*" element={<p>404 - Not found</p>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
